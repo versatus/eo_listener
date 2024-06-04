677b540ecc69188f207bd7337638e5f1090828f7
andrewvious <106849954+andrewvious@users.noreply.github.com>
2024-06-04 16:44:43 -0500

    Update Kaleido URL (#7)
    update URL using new node

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), EoServerError> {
         .map_err(|e| EoServerError::Other(e.to_string()))?;
 
     let http: Http =
-        Http::new("https://u0sb67wcu2:oqyk-7M8zc7F08HkDr48_Bk1TuZIcjzpC9on7ST-yWQ@u0d79gh32z-u0o7pt95od-rpc.us0-aws.kaleido.io/").map_err(|err| EoServerError::Other(err.to_string()))?;
+        Http::new("https://u0wg2s6qkr:iFquXU0AjYTJmtFRaP2gahIOUNHTH5oIuJyfX9ItUI4@u0auvfalma-u0j1mdxq0w-rpc.us0-aws.kaleido.io/").map_err(|err| EoServerError::Other(err.to_string()))?;
     let web3: Web3<Http> = Web3::new(http);
 
     let path = "./blocks_processed.dat";
